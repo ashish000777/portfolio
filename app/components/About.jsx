17ed6b9b226730d742106fd3d4c16d4661a619bc
ashish000777 <ashishbagate03@gmail.com>
2025-02-23 21:56:26 +0530

    Added tools

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,4 +1,4 @@
-import { assets, infoList } from '@/assets/assets'
+import { assets, infoList, toolsData } from '@/assets/assets'
 import React from 'react'
 import Image from 'next/image'
 
@@ -18,11 +18,19 @@ const About = () => {
 
           <ul className='grid grid-cols-1 sm:grid-cols-3 gap-6 max-w-2xl'>
             {infoList.map(({icon, iconDark, title, description}, index)=>(
-              <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500' 
+              <li className='border-[0.5px] border-gray-400 rounded-xl p-6 cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500 hover:shadow-black' 
               key={index}>
                 <Image src={icon} alt={title} className='w-7 mt-3'/>
                 <h3 className='my-4 font-semibold text-gray-700'>{title}</h3>
                 <p className='text-gray-600 text-sm'>{description}</p>
+              </li>
+            ))}
+          </ul>
+          <h4>Tools I use</h4>
+          <ul>
+            {toolsData.map((tool, index)=>(
+              <li key={index}>
+                <Image src={tool} alt='Tool' className='w-5 sm:w-7' />
               </li>
             ))}
           </ul>
